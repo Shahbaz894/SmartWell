05f12e7f8d0cf50dcb51585d0997ebae39e6ed42
shahbaz <shahbazzulfiqar894@gmail.com>
2026-04-08 19:01:35 +0500

    update motor tementary schema

--- app/schemas/motor_telemetry_schema.py
+++ app/schemas/motor_telemetry_schema.py
@@ -1,33 +1,22 @@
 from pydantic import BaseModel
-
 from datetime import datetime
 
-
 class MotorTelemetryBase(BaseModel):
-
-    output_frequency: float
-    reference_frequency: float
-
-    dc_bus_voltage: float
-    output_voltage: float
-    output_current: float
-
-    motor_speed: float
-
-    power_load: float
-    torque_load: float
-
-    real_power: float
-
+ output_frequency: float
+ reference_frequency: float
+ dc_bus_voltage: float
+ output_voltage: float
+ output_current: float
+ motor_speed: float
+ power_load: float
+ torque_load: float
+ real_power: float
 
 class MotorTelemetryCreate(MotorTelemetryBase):
-    device_id: str
-
+ device_id: str
 
 class MotorTelemetryResponse(MotorTelemetryBase):
-    id: str
-    device_id: str
-    created_at: datetime
-
-    class Config:
-        from_attributes = True
+ id: str
+ device_id: str
+class Config:
+ from_attributes = True
